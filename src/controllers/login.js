fd43d75c3c39df1ed5f7a965752423b32f1a8164
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-11-20 19:20:52 -0300

    Make upsert for sessions

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -34,16 +34,16 @@ const makeLogin = async (req, res) => {
 			return res.status(401).send('E-mail e/ou senha incorretos(s)!')
 		}
 
-		const token = uuid()
-		await makeSession(userData.id, token)
+		const token = await makeSession(userData.id)
 
 		const sendInfo = {
 			name: userData.name,
 			email: lowerEmail,
 			token,
+			isSubscriber: userData.is_subscriber
 		}
 
-		res.status(200).send(sendInfo)
+		return res.status(200).send(sendInfo)
 
 	} catch (error) {
 		console.log(error)
@@ -61,14 +61,43 @@ const registeredUser = async (email) => {
 	return registeredUserPromise.rows[0]
 }
 
-const makeSession = async (userId, token) => {
-	const query = `
-		INSERT INTO sessions
-			(user_id, token)
-		VALUES
-			($1, $2);
-	`
-	await connection.query(query, [userId, token])
+const makeSession = async (userId) => {
+	const token = uuid()
+	
+	const existentSessionId = await existentSession(userId)
+	
+	existentSessionId
+		? await updateSession(existentSessionId, token)
+		: await insertSession(userId, token)
+	
+	return token
+}
+
+const existentSession = async (userId) => {
+	const existentUserPromise = await connection.query(`
+    SELECT * FROM sessions
+      WHERE user_id = $1;
+  `, [userId])
+	const existentUser = existentUserPromise.rows[0]
+
+	return existentUser?.id
+}
+
+const updateSession = async (sessionId, token) => {
+	await connection.query(`
+  UPDATE sessions
+  SET token = $1
+    WHERE id = $2;
+  `, [token, sessionId])
+}
+
+const insertSession = async (userId, token) => {
+	await connection.query(`
+    INSERT INTO sessions
+      (user_id, token)
+    VALUES
+      ($1, $2);
+  `, [userId, token])
 }
 
 
