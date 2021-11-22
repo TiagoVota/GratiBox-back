072ebfaa7d5ec1391d26be10650d308d75a7c7a1
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2021-11-22 16:44:55 -0300

    Create populate database sql file

---
+++ populate_db.sql
@@ -0,0 +1,13 @@
+insert into plans (name) values ('semanal');
+insert into plans (name) values ('mensal');
+
+insert into products (name) values ('Chás');
+insert into products (name) values ('Produtos Orgânicos');
+insert into products (name) values ('Incensos');
+
+insert into frequencies (week_day, month_day) values (1, NULL);
+insert into frequencies (week_day, month_day) values (3, NULL);
+insert into frequencies (week_day, month_day) values (5, NULL);
+insert into frequencies (week_day, month_day) values (NULL, 1);
+insert into frequencies (week_day, month_day) values (NULL, 10);
+insert into frequencies (week_day, month_day) values (NULL, 20);
